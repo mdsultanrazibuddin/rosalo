eee029f9e135f11f6d1fccc61393823a60e686a2
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-09-03 16:12:37 +0600

    added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Home/Navbar/Navbar";
+import Footer from "../Pages/Home/Footer/Footer";
 
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
