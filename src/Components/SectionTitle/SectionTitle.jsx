fccafa4824f28202a113b548d1f0e430dbf31518
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-09-05 12:05:13 +0600

    added

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({heading, subheading}) => {
     return (
-        <div>
-            <p>{subheading}</p>
-            <h3>{heading}</h3>
+        <div className='mx-auto text-center md:w-4/12 my-8'>
+            <p className='text-yellow-600 mb-2'>--- {subheading} ---</p>
+            <h3 className='text-3xl uppercase border-y-4 py-4 '>{heading}</h3>
         </div>
     );
 };
